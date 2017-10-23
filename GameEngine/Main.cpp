9b5d0284f6fd9183ef203ec5d1a391f21c38ba7f
ckchessmaster <Genesis1:1>
2017-10-23 13:31:38 -0400

    testing

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,25 +1,42 @@
-#include "Main.h"
-#include <SFML/Graphics.hpp>
+//#include "Main.h"
+//#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <chrono>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//sf::RenderWindow window(sf::VideoMode(1600, 900), "Game Engine");
+	//sf::CircleShape shape(100.f);
+	//shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+	//// run the program as long as the window is open
+	//while (window.isOpen())
+	//{
+	//	// check all the window's events that were triggered since the last iteration of the loop
+	//	sf::Event event;
+	//	while (window.pollEvent(event))
+	//	{
+	//		// "close requested" event: we close the window
+	//		if (event.type == sf::Event::Closed)
+	//			window.close();
+	//	}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+	//	window.clear();
+	//	window.draw(shape);
+	//	window.display();
+	//}
+
+	using namespace std::chrono;
+	std::cout << "Running sim...\n";
+
+	int i = 0, endTime;
+	int startTime = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
+
+	while (i < 1000000000) {
+		i++;
 	}
+	endTime = duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
+	std::cout << "Total time: " << (endTime - startTime) << "ms\nFinal number: " << i << "\n";
 
 	return 0;
 }
